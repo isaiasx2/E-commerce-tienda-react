4409e69893d386f173a3cd74754c034b8c128254
isaiasx2 <isaias.luquez@gmail.com>
2022-06-11 23:38:25 -0300

    entrega 11/06/2022

---
+++ src/Components/ItemCount/ItemCount.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+
+
+const useCounter = ()=>{
+    const [counter, setCounter] = useState(1);
+
+    const sumar = () => setCounter(counter + 1);
+    const restar = () => setCounter(counter -1);
+    
+
+    return(
+        {
+        counter,
+        sumar,
+        restar,
+        
+        }
+)}
+
+
+export const ItemCount = ({initial, stock, onAdd})=>{
+    
+    
+    const {
+        counter,
+        sumar,
+        restar,
+        } = useCounter(initial);
+    
+    
+    
+    return (
+        
+        <div className='contador'>
+            <h2>Ingresa cantidad</h2>
+        
+            <button disabled={counter <= 1} onClick={restar}>-</button>
+            <span>{counter}</span>
+            <button disabled={counter >= stock} onClick={sumar}>+</button>
+            <>
+                <button disabled={stock <= 0} onClick={()=>onAdd(counter)}>Agregar al Carrito</button>
+            </>
+            
+        </div>   
+        
+    )
+    
+
+}
